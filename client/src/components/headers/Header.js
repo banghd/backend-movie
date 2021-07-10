fa719142bf4fb329edce6da7d393c1b05fe5d537
banghd <47983625+banghd@users.noreply.github.com>
2021-07-10 15:38:26 +0700

    Update Header.js

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -55,7 +55,7 @@ function Header() {
 
             <div className="logo">
                 <h1>
-                    <Link to="/">{isAdmin ? 'Admin' : 'DevAT Shop'}</Link>
+                    <Link to="/">{isAdmin ? 'Admin' : 'Eccomere'}</Link>
                 </h1>
             </div>
 
