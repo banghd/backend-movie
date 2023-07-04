329254fd1a54f167bf0fccff117e524889d6b2f0
dmbang <bang@jambowallet.com>
2023-07-04 12:10:54 +0700

    enable cors

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.use(fileUpload({
     useTempFiles: true
 }))
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 // Routes
 app.use('/user', require('./routes/userRouter'))
 app.use('/api', require('./routes/categoryRouter'))
